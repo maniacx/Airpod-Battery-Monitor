514339d674a66f46903c123d78e29733c3c39e1f
maniacx <macs18max@gmail.com>
2024-02-23 18:05:45 -0800

    cleanup: Remove unneeded comments

--- extension.js
+++ extension.js
@@ -3,8 +3,6 @@ import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as BluetoothClient from './lib/bluetoothClient.js';
 
 export default class AirpodBatteryMonitorExtension extends Extension {
-    // Bluetooth quicks settings menu are accessible when the session is locked.
-    // Therefore unlock-dialog session mode is used.
     enable() {
         this._settings = this.getSettings();
         this._bluetoothClient = new BluetoothClient.BluetoothClient(this, this._settings);
